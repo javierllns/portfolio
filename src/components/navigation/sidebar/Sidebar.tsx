857f4344cbb2a3f1fa75ab2370b2df8f1c358164
javierllns <javier.ll.m@outlook.com>
2022-12-12 15:51:00 -0500

    added few custom fonts

--- src/components/navigation/sidebar/Sidebar.tsx
+++ src/components/navigation/sidebar/Sidebar.tsx
@@ -1,7 +1,14 @@
 import styles from "./Sidebar.module.css";
 
 export const Sidebar = () => {
-  return <div className={styles.mainContainer}>Sidebar</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.profilePicture}></div>
+      <div className={styles.profileName}>Javier Llanos</div>
+      <div className={styles.socialLinks}></div>
+      <div className={styles.siteNavigation}></div>
+    </div>
+  );
 };
 
 export default Sidebar;
