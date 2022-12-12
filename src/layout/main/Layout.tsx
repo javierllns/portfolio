a12c4073f25b297291aae3dfedf9dda3d5e5bb4b
javierllns <javier.ll.m@outlook.com>
2022-12-12 02:42:43 -0500

    WIP - added css normalizer - working in sidebar nav

--- src/layout/main/Layout.tsx
+++ src/layout/main/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import styles from "./Layout.module.css";
 
 export const MainLayout = () => {
   return (
-    <div>
-      <div>Main layout</div>
-      <Outlet />
+    <div className={styles.mainContainer}>
+      <div style={{ height: "100vh", width: "200px", backgroundColor: "red" }}>Main layout</div>
+      <div className={styles.content}>
+        <Outlet />
+      </div>
     </div>
   );
 };
